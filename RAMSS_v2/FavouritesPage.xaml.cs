ddfd118ee140efe4654ba5fb22a7e0bf86277676
Andyll96 <andysl_1996@hotmail.com>
2016-11-22 14:09:29 -0500

    Home Page
    Home Page 90% Complete

--- RAMSS_v2/FavouritesPage.xaml.cs
+++ RAMSS_v2/FavouritesPage.xaml.cs
@@ -13,24 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace RAMSS_v2
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class FavouritesPage : Page
     {
-        public MainPage()
+        public FavouritesPage()
         {
             this.InitializeComponent();
-            myFrame.Navigate(typeof(HomePage));
-        }
-
-        private void homeButton_Click(object sender, RoutedEventArgs e)
-        {
-            myFrame.Navigate(typeof(HomePage));
         }
     }
 }
---
+++ RAMSS_v2/HomePage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RAMSS_v2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void courseScheduleButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(CourseSchedulePage));
+        }
+
+        private void studentFeesButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(FinancialPage));
+        }
+
+        private void academicsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AcademicsPage));
+        }
+
+        private void favouritesButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(FavouritesPage));
+        }
+
+        private void alertsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AlertsPage));
+        }
+    }
+}
